7f31c72a1b85214bfad562e3febb95cf014f1834
endows <you@example.com>
2015-07-06 13:07:50 +0900

    Material UIをインストールする

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+var React = require('react')
+var Name = React.createClass({
+  render: function() {
+    return (
+      <span>yahho?</span>
+    );
+  }
+});
+
+React.render(
+  <Name />,
+  document.body
+);
